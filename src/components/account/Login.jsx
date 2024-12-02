d9f4d3e15de2524a15d68962055fff5dd90c80d7
Akshay <akshaydivakaran01@gmail.com>
2024-12-02 23:01:49 +0530

    Initial commit

--- src/components/account/Login.jsx
+++ src/components/account/Login.jsx
@@ -1,12 +1,37 @@
 import React from 'react'
+import "./Login.css"
 
 import Box from '@mui/material/Box';
+import FormControl from '@mui/joy/FormControl';
+import FormLabel from '@mui/joy/FormLabel';
+import Input from '@mui/joy/Input';
+import logo from '../../assets/Logo.png';
 
 const Login = () => {
   return (
-    <Box>
-        <h1>Login</h1>
-    </Box>
+    <div className="container">
+      <Box>
+          <img src={logo} alt="Logo"/>
+          <br /><br /><br /><br />
+          <FormControl>
+            <FormLabel>Email</FormLabel>
+            <Input
+              name="email"
+              type="email"
+              placeholder="johndoe@email.com"
+            />
+          </FormControl>
+          <br /><br />
+          <FormControl>
+            <FormLabel>Password</FormLabel>
+            <Input
+              name="password"
+              type="password"
+              placeholder="password"
+            />
+          </FormControl>
+      </Box>
+    </div>
   )
 }
 
