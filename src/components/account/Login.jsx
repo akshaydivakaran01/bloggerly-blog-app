b8b3b47ed5d9966d6d34f7e954509c4478d25171
Akshay <akshaydivakaran01@gmail.com>
2024-12-03 21:57:18 +0530

    Initial commit

--- src/components/account/Login.jsx
+++ src/components/account/Login.jsx
@@ -1,37 +1,45 @@
 import React from 'react'
-import "./Login.css"
 
-import Box from '@mui/material/Box';
-import FormControl from '@mui/joy/FormControl';
-import FormLabel from '@mui/joy/FormLabel';
-import Input from '@mui/joy/Input';
+import { Box, Button, TextField } from '@mui/material';
+import { FormControl, FormLabel } from '@mui/joy';
+import styled from '@emotion/styled';
+
 import logo from '../../assets/Logo.png';
+
+const Component = styled(Box)`
+  width: 400px;
+  margin: auto;
+  box-shadow: 5px 2px 5px 2px rgb(0 0 0/ 0.6);
+`;
+
+const Image = styled('img')({
+  width: 350,
+  margin: 'auto',
+  display: 'flex',
+  padding: '50px 0 0'
+});
+
+const Wrapper = styled(Box)`
+  padding: 25px 35px;
+  margin: auto;
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+`;
 
 const Login = () => {
   return (
-    <div className="container">
-      <Box>
-          <img src={logo} alt="Logo"/>
-          <br /><br /><br /><br />
-          <FormControl>
-            <FormLabel>Email</FormLabel>
-            <Input
-              name="email"
-              type="email"
-              placeholder="johndoe@email.com"
-            />
-          </FormControl>
-          <br /><br />
-          <FormControl>
-            <FormLabel>Password</FormLabel>
-            <Input
-              name="password"
-              type="password"
-              placeholder="password"
-            />
-          </FormControl>
-      </Box>
-    </div>
+      <Component>
+        <Box>
+          <Image src={logo} alt="Logo" width="350"/>
+          <Wrapper>
+            <TextField id="email" label="Email" variant="standard" required />
+            <TextField id="passowrd" label="Password" variant="standard" type="password" autoComplete="current-password" required />
+            <Button variant="contained">Login</Button>
+            <Button>Create an Account</Button>
+          </Wrapper>
+        </Box>
+      </Component>
   )
 }
 
