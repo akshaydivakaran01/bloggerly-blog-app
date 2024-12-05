02bf9c482eada3fdcd4a2d47fdd73d110a803c1c
Akshay <akshaydivakaran01@gmail.com>
2024-12-05 23:20:36 +0530

    Initial commit

--- src/components/account/Login.jsx
+++ src/components/account/Login.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
+import { useState } from 'react';
 
 import { Box, Button, TextField, Typography } from '@mui/material';
 import styled from '@emotion/styled';
 
 import logo from '../../assets/Logo.png';
 
+
 const Component = styled(Box)`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
+
+const Container = styled(Box)`
   width: 400px;
-  margin: auto;
   border-radius: 10px;
   box-shadow: 5px 2px 5px 2px rgb(0 0 0/ 0.6);
 `;
@@ -30,7 +39,7 @@ const Wrapper = styled(Box)`
   }
 `;
 
-const LoginButton = styled(Button)`
+const Button1 = styled(Button)`
   text-transform: none;
   background: #000;
   color: #FFF;
@@ -39,7 +48,7 @@ const LoginButton = styled(Button)`
   font-weight: 500;
 `;
 
-const SignUpButton = styled(Button)`
+const Button2 = styled(Button)`
   text-transform: none;
   background: #FFF;
   color: #000;
@@ -54,20 +63,45 @@ const Text = styled(Typography)`
 `
 
 const Login = () => {
+
+  const [signup, IsSignup] = useState(false);
+
+  const toggleSignup = () => {
+    IsSignup(!signup)
+  }
   return (
       <Component>
-        <Box>
+        <Container>
           <Image src={logo} alt="Logo" width="350"/>
-          <Wrapper>
-            <TextField id="email" label="Email" variant="standard" required />
-            <TextField id="passowrd" label="Password" variant="standard" type="password" autoComplete="current-password" required />
-            <LoginButton variant="contained">Login</LoginButton>
-            <Text style={{textAlign: 'center'}}>
-              OR
-            </Text>
-            <SignUpButton>Create an Account</SignUpButton>
-          </Wrapper>
-        </Box>
+
+          { 
+            signup === false ?
+              <Wrapper>
+                <TextField id="email" label="Email" variant="standard" required />
+                <TextField id="passowrd" label="Password" variant="standard" type="password" autoComplete="current-password" required />
+                <Button1 variant="contained">Login</Button1>
+                <Text style={{textAlign: 'center'}}>
+                  OR
+                </Text>
+                <Button2 onClick={() => {
+                  toggleSignup()
+                }}>Create an Account</Button2>
+              </Wrapper> :
+
+            <Wrapper>
+              <TextField id="name" label="Name" variant="standard" required />
+              <TextField id="email" label="Email" variant="standard" required />
+              <TextField id="passowrd" label="Password" variant="standard" type="password" autoComplete="current-password" required />
+              <Button1 variant="contained">Signup</Button1>
+              <Text style={{textAlign: 'center'}}>
+                OR
+              </Text>
+              <Button2 onClick={() => {
+                  toggleSignup()
+                }}>Already have an Account</Button2>
+            </Wrapper>
+          }
+        </Container>
       </Component>
   )
 }
